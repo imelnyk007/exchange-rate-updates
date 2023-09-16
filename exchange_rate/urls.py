e91f5024dc811c0297fa8cf346be4ee18bfd9bf1
irynamelnyk <melnyk7iryna@gmail.com>
2023-09-16 12:12:53 +0300

    Rename url

--- exchange_rate/urls.py
+++ exchange_rate/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from exchange_rate.views import record_exchange_rate_to_excel, UserCreateView, TokenCreateView
+from exchange_rate.views import record_exchange_rate_to_excel, TokenCreateView
 
 urlpatterns = [
-    path("", record_exchange_rate_to_excel,  name="exchange_rate"),
-    path("register/", UserCreateView.as_view(), name="create"),
+    path("update/", record_exchange_rate_to_excel,  name="update"),
     path('token/', TokenCreateView.as_view(), name="token"),
 ]
 
