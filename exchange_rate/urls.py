f34d98cc21878f902822fdcd6f7a59788fe86efd
irynamelnyk <melnyk7iryna@gmail.com>
2023-09-18 12:14:57 +0300

    Fix flake8

--- exchange_rate/urls.py
+++ exchange_rate/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from exchange_rate.views import record_exchange_rate_to_excel, TokenCreateView
 
 urlpatterns = [
-    path("update/", record_exchange_rate_to_excel,  name="update"),
-    path('token/', TokenCreateView.as_view(), name="token"),
+    path("update/", record_exchange_rate_to_excel, name="update"),
+    path("token/", TokenCreateView.as_view(), name="token"),
 ]
 
 app_name = "exchange_rate"
